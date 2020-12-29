dc7db364e3749c0bf4728226a5afa64b64c48c89
Dominik Bullo <dominobullo@gmail.com>
2020-12-29 16:51:20 +0100

    feat: StandardTurret upgrading work

--- Assets/Scripts/NodeUI.cs
+++ Assets/Scripts/NodeUI.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NodeUI : MonoBehaviour
 {
@@ -8,10 +9,25 @@ public class NodeUI : MonoBehaviour
     public GameObject ui;
     private Node target;
 
+    public Text upgradeCost;
+    public Button upgradeButton;
+
     public void SetTarget(Node _target)
     {
         target = _target;
         transform.position = target.GetBuildPosition();
+
+        if (!target.isUpgraded)
+        {
+            upgradeCost.text = target.turretBlueprint.upgradeCost + "€";
+            upgradeButton.interactable = true;
+        }
+        else
+        {
+            upgradeCost.text = "DONE";
+            upgradeButton.interactable = false;
+        }
+
         ui.SetActive(true);
     }
 
